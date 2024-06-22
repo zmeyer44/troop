bdff5d1d1b8676cfc8c5e4fe96349c58eba4ae7b
zmeyer44 <zmmeyer44@gmail.com>
2024-06-22 08:17:22 -0400

    testing

--- apps/web/components/ui/tooltip.tsx
+++ apps/web/components/ui/tooltip.tsx
@@ -2,10 +2,10 @@
 
 import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
-
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { cn } from "@/lib/utils";
 
-const TooltipProvider = TooltipPrimitive.Provider;
+// const TooltipProvider = TooltipPrimitive.Provider;
 
 const Tooltip = TooltipPrimitive.Root;
 
